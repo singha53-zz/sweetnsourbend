07fcf22292dc6be9083485085753e0ded9e36ad0
Abdul Said <abdulrahman.asaid@gmail.com>
2018-09-08 13:21:44 -0400

    added parser

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var data = require('./data.js')
+var data = require('./data.js');
 
 var waitingArryay = data.waitingArray;
 var tableArray = data.tableArray;
 
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get("/tables", function(req, res){
-  res.json(tableArray)
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/tables', function(req, res) {
+  res.json(tableArray);
+});
 
 app.get('/', function(req, res) {
   res.send('Hello afkdjakfljalfjlkajglkjglkajglkdjworld');
